5eb73c2aafb6fc5534fcc33382e6ab538ca6be97
Georginho <61295220+DOPEKILL431@users.noreply.github.com>
2022-04-20 18:32:37 +0000

    Update MAIL-AND-PASSWORD GENERATOR.py

--- MAIL-AND-PASSWORD GENERATOR.py
+++ MAIL-AND-PASSWORD GENERATOR.py
@@ -1,26 +1,13 @@
 import random
 from re import U
 
-nom = str(input("Saisissez votre nom :"))
-prenom = str(input("Saisissez votre prénom :"))
-annee = int(input("saisissez votre année de naissance : "))
-reste = "@gmail.com"
-
-LIST_MAIL=[]
-LIST_MAIL.append(nom+prenom+str(annee)+reste)
-
-
-
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+majuscule = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 chiffre  = "123456789"
-special  = "~#{[[|`\^@]}£¨%µ§/.?@"
-ensemble = alphabet + alphabet + alphabet + chiffre + special 
-
-
-
+special  = "@[]^-!#$£%&()*+,-./:;{}<>=~?"
+ensemble = alphabet + chiffre + special + majuscule
 
 lenght = int(input("Definissez la longueur de votre mot de passe : "))
-
 mdp_gen = []
 
 for i in range(lenght) : 
@@ -29,13 +16,9 @@ for i in range(lenght) :
 for i in range (10) : 
     print("")
 
-
-
-
 print(" ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ ____ ____ ____ ____ ")
 print("||M |||A |||I |||L |||       |||G |||E |||N |||E |||R |||A |||T |||O |||R ||")
 print("||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__|||__|||__|||__|||__||")
 print("|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|")
-
-print("votre adresse mail est :","".join(LIST_MAIL))
+print("")
 print("Votre mot de passe est : ","".join(mdp_gen))  
